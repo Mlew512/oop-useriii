2f158fab321a4f1e172697f78777a5cc8db868f5
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-12 21:42:43 +0200

    added user class

--- users/User.py
+++ users/User.py
@@ -1,1 +1,60 @@
-# Your User class goes here
+class User:
+    posts = {}
+    post_counter = 0
+
+    def __init__(
+        self, name, email_address, drivers_license_number="unknown"
+    ):
+        self._name = name 
+        self._email = email_address 
+        self._dln = drivers_license_number  
+        self._friends = 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def dln(self):
+        return self._dln
+
+    @property
+    def friends(self):
+        return self._friends
+
+    def __str__(self):
+        return f"{self.name}'s email is {self.email} and drivers license number is {self.dln}"
+
+    @friends.setter 
+    def set_friends(self, new_friends_number):
+        if new_friends_number > 0:
+            self._friends = new_friends_number
+
+    def create_post(self):
+        User.post_counter += 1
+        post = input("what would you like to post? : ...")
+        new_post = f"{self._name} : {post}"
+        User.posts[User.post_counter] = new_post
+        print(User.posts)
+
+    @classmethod
+    def delete_post(cls, post_number):
+        for obj in cls.posts:
+            if obj == post_number:
+                cls.posts[post_number] = "deleted"
+        print(cls.posts)
+
+
+# user_1= User("tom", "tom@myspace.com", 2003030)
+# mclovin = User("McLovin", "mclovin@hawaii.gov", 14787441)
+# mclovin.create_post()
+# user_1.create_post()
+# # is using __str__ method
+# # print(mclovin)#print of the self will use the string method
+# # print(user_1)
+# User.delete_post(1)
+# # Your User class goes here
